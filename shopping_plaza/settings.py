f8ac78f3887f924cd8e26c0d10cee091c9fd5de1
soniitsa <ojosonia@yahoo.com>
2021-02-18 20:25:38 +0000

    Added a shirt on admin

--- shopping_plaza/settings.py
+++ shopping_plaza/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '&51+_219@rsfj$(ir-*ktm5unuyu&oz7k5%kylse*o#q*kc_4i'
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
