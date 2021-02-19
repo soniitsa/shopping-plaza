1d2a11368bf1693d8414a8951d8c1b35476343b3
soniitsa <ojosonia@yahoo.com>
2021-02-19 16:32:50 +0000

    Added secret key environ

--- shopping_plaza/settings.py
+++ shopping_plaza/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&51+_219@rsfj$(ir-*ktm5unuyu&oz7k5%kylse*o#q*kc_4i'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 
