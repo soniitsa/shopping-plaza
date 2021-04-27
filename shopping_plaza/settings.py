a2031fcc90fef8b103a74a71ba79a7262299a948
soniitsa <ojosonia@yahoo.com>
2021-04-27 16:49:52 +0000

    deployment part 5

--- shopping_plaza/settings.py
+++ shopping_plaza/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fashion-plaza.herokuapp.com', 'localhost']
 
