16a3129f37734b92f4cc3ed3d580e8858a18eee2
soniitsa <ojosonia@yahoo.com>
2021-04-28 11:15:35 +0000

    fixing images

--- shopping_plaza/settings.py
+++ shopping_plaza/settings.py
@@ -189,11 +189,10 @@ if 'USE_AWS' in os.environ:
 if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fashion.plaza'
-    AWS_S3_REGION_NAME = 'eu-central-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
+    AWS_S3_CUSTOM_DOMAIN = f's3.{AWS_S3_REGION_NAME}.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}'
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
